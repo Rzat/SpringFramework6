6bdd0f83b45c58f4d7c5982f0f262f4f9f4ec2ee
Rzat <rajatthakur00@gmail.com>
2023-03-25 22:16:46 +0000

    intializing base

--- src/main/java/com/poc/springframework6/repositories/BookRepository.java
+++ src/main/java/com/poc/springframework6/repositories/BookRepository.java
@@ -1,2 +1,7 @@
-package com.poc.springframework6.repositories;public interface BookRepository {
+package com.poc.springframework6.repositories;
+
+import com.poc.springframework6.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository  extends CrudRepository<Book, Long> {
 }
