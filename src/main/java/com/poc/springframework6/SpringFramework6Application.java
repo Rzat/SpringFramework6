9939b2d53efbb153afbbc7a73f473efabeaa0c41
Rzat <rajatthakur00@gmail.com>
2023-03-25 21:58:18 +0000

    first commit

---
+++ src/main/java/com/poc/springframework6/SpringFramework6Application.java
@@ -0,0 +1,13 @@
+package com.poc.springframework6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramework6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFramework6Application.class, args);
+    }
+
+}
